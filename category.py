b6fbc2f4a8504ea993351a928e66c1005feb1c74
Karim-Dorado <karimdorado@gmail.com>
2021-01-31 09:21:03 +0100

    Revue de code

--- category.py
+++ category.py
@@ -38,4 +38,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Scrape a Category')
     parser.add_argument('--url', type=str, metavar='', help='URL of a category')
     args = parser.parse_args()
-    print(scrape_cat(args.cat))
+    print(scrape_cat(args.url))
